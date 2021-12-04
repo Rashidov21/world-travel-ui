86be87d3089fcbcdbea3ee051bce093b630f6dd7
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-12-04 23:57:20 +0500

    banner area design

--- js/core.js
+++ js/core.js
@@ -1,1 +1,8 @@
 M.AutoInit();
+let tech = document.querySelectorAll(".tech");
+for(let i = 0; i < tech.length; i++){
+    tech[i].style.width = `${Math.round(50 - Math.random() * 150)}px`;
+    tech[i].style.top = `${Math.round(Math.random() * 500)}px`;
+    tech[i].style.left = `${Math.round(Math.random() * 500)}px`;
+    tech[i].style.transition = `all 0.5s`;
+}
