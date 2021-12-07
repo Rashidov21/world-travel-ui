7115777dc1566bc06bd6107372212e5e3858ad54
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-12-07 21:25:36 +0500

    some was change

--- js/core.js
+++ js/core.js
@@ -8,3 +8,14 @@ var instances = M.Carousel.init(elems, {
     dist:-500,
     shift:200
 });
+
+var fbButton = document.getElementById('fb-share-button');
+var url = window.location.href;
+
+fbButton.addEventListener('click', function() {
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
+        'facebook-share-dialog',
+        'width=800,height=600'
+    );
+    return false;
+});
