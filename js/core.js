35659e09fe25a65405bd3fa8cb63dc8caec6f1cc
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2021-12-10 17:53:16 +0500

    css and core.js

--- js/core.js
+++ js/core.js
@@ -1,21 +1,4 @@
-M.AutoInit();
-var elems = document.querySelectorAll('.carousel');
-var instances = M.Carousel.init(elems, {
-    duration:400,
-    fullWidth:true,
-    indicators:true,
-    noWrap:false,
-    dist:-500,
-    shift:200
-});
+const showNavbarMenu = anime({
+    targets:".navbar-burger",
 
-var fbButton = document.getElementById('fb-share-button');
-var url = window.location.href;
-
-fbButton.addEventListener('click', function() {
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
-        'facebook-share-dialog',
-        'width=800,height=600'
-    );
-    return false;
 });
