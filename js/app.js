7fb7f67b494f3e689c5c6d6730f96151c5550265
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-06-29 16:07:35 +0500

    add full page

--- js/app.js
+++ js/app.js
@@ -1,46 +1,13 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    autoplay: {
-        delay: 5000,
-    },
-    loop: true,
-    autoHeight: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+const page = new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    easing: 'easeInOutCubic',
+    keyboardScrolling: true,
+    animateAnchor: true,
+    recordHistory: true,
+    parallax: true,
+    scrollBar: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
 });
-
-
-
-
-
-
-setInterval(function() {
-    let banner = document.querySelector("#banner")
-    let images = ["../src/samarkand.jpg", "../src/xiva.jpg", "../src/samarkand.jpg"]
-    let index = 0
-
-    console.log(banner)
-    if (index > images.length) {
-        index = 0
-    } else {
-        index++
-        banner.style.backgroundImage = `url(${images[index]})`
-    }
-
-    console.log(index)
-}, 3000)
